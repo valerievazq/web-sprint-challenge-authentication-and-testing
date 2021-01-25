4e407572217641eb8d1ca825aa0f87a8e03eb50b
Valerie <53797814+valerievazq@users.noreply.github.com>
2021-01-24 19:07:14 -0700

    final touches in the making

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -17,13 +17,13 @@ module.exports = (req, res, next) => {
   const token = req.headers.authorization;
 
   if (!token) {
-    return res.status(401).json({ message: "token required" });
+    return res.status(401).json({ message: "you are up to no good" });
   }
 
   jwt.verify(token, jwtSecret, (err, decoded) => {
     if (err) {
       console.log(err);
-      return res.status(401).json({ message: "token invalid" });
+      return res.status(401).json({ message: "You shall not pass" });
     }
 
     console.log(decoded);
